db2472ab2f73416df44b3737f07645dcbdc73d8e
Jacob Schuster <jschuster8765@gmail.com>
2023-11-30 21:31:36 -0500

    better labels

--- src/components/Search/AuxGraphs/MaterialDensityStrengthGraph.tsx
+++ src/components/Search/AuxGraphs/MaterialDensityStrengthGraph.tsx
@@ -1,12 +1,15 @@
+import { CEMENT_TYPE_COLOR_MAPPING } from "@/constants/dbProperties";
 import { Material } from "@/types/entities";
 import { useMemo } from "react";
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, LabelList } from "recharts";
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, LabelList, Cell, TooltipProps } from "recharts";
+import { NameType, ValueType } from "recharts/types/component/DefaultTooltipContent";
 
 interface ScatterPlotData {
   x: number;
   y: number;
   name: string;
   mixName: string;
+  color: string;
 }
 
 export default function MaterialDensityStrengthGraph({ materials }: { materials: Material[] | undefined }) {
@@ -28,6 +31,7 @@ export default function MaterialDensityStrengthGraph({ materials }: { materials:
           y: strengthAmount as number,
           name: material.name,
           mixName: mix.name,
+          color: CEMENT_TYPE_COLOR_MAPPING[material.metadata.formulaName] ?? "#8884d8",
         });
       });
     });
@@ -37,6 +41,23 @@ export default function MaterialDensityStrengthGraph({ materials }: { materials:
   }, [materials]);
 
   if (!graphData) return null;
+
+  const CustomTooltip = ({ active, payload, label }: TooltipProps<ValueType, NameType>) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="rounded-sm bg-white p-2">
+          <p className="text-md">{`${payload[0]?.payload.name}, ${payload[0]?.payload.mixName}`}</p>
+          {payload.map((entry: any, i) => (
+            <div key={i}>
+              <p className="text-sm">{`${entry.name} : ${entry.value}${entry.unit}`}</p>
+            </div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
+  };
 
   return (
     <div className="mt-4 rounded-lg bg-ivory p-4">
@@ -53,12 +74,25 @@ export default function MaterialDensityStrengthGraph({ materials }: { materials:
           <CartesianGrid />
           <XAxis type="number" dataKey="x" name="Density" unit="g" />
           <YAxis type="number" dataKey="y" name="Strength" unit="MPa" />
-          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-          <Scatter name="A school" data={graphData} fill="#8884d8">
-            <LabelList dataKey="name" position="bottom" />
+          <Tooltip content={<CustomTooltip />} />
+          <Scatter name="A school" data={graphData}>
+            {graphData.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={entry.color ?? "#8884d8"} />
+            ))}
+            {/* <LabelList dataKey="x" /> */}
+            {/* <LabelList dataKey="name" position="bottom" /> */}
           </Scatter>
         </ScatterChart>
       </ResponsiveContainer>
+
+      <div className="mt-1 flex flex-row justify-between">
+        {Object.entries(CEMENT_TYPE_COLOR_MAPPING).map(([key, value], i) => (
+          <div className="flex flex-row items-center" key={i}>
+            <div className="mr-2 h-4 w-4 rounded-full" style={{ backgroundColor: value }}></div>
+            <p className="text-sm">{key}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
